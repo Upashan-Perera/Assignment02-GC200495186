3688bf89da27341bb4ab70f6a0d12afd52566bca
Upashan-Perera <96879265+Upashan-Perera@users.noreply.github.com>
2022-11-20 19:58:11 -0500

    connecting APIs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.assignment02gc200495186 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires com.google.gson;
 
 
-    opens com.example.assignment02gc200495186 to javafx.fxml;
+    opens com.example.assignment02gc200495186 to javafx.fxml, com.google.gson;
     exports com.example.assignment02gc200495186;
 }
