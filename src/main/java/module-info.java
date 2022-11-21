a422417f6eb6c7339ecb96042196d77d6e37805a
Upashan-Perera <96879265+Upashan-Perera@users.noreply.github.com>
2022-11-20 19:51:27 -0500

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment02gc200495186 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment02gc200495186 to javafx.fxml;
+    exports com.example.assignment02gc200495186;
+}
